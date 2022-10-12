224b0caae7df200a27720babcb0cd0d133510f05
Jason Taylor <jasonleetaylor88@gmail.com>
2022-10-12 07:15:59 +0100

    feat: wire up a minimalist front end for demonstration

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 module.exports = {
 	mapboxBaseURL: "https://api.mapbox.com/geocoding/v5",
 	weatherstackBaseUrl: "http://api.weatherstack.com/current",
+	frontEndBaseUrl: "localhost:3000",
 	weather_units: "f",
 };
-
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
 console.log("Client side javascript file is loaded");
+
+const weatherform = document.querySelector("form");
+const search = document.querySelector("form input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
+
+messageOne.textContent = "";
+messageTwo.textContent = "";
+
+weatherform.addEventListener("submit", async e => {
+	e.preventDefault();
+	const location = search.value;
+	messageOne.textContent = "Loading...";
+	messageTwo.textContent = "";
+
+	fetch(`http://localhost:3000/weather?address=${location}`)
+		.then(response => {
+			response.json().then(data => {
+				if (data.error) {
+					messageOne.textContent = "";
+					messageTwo.textContent = data.error;
+				} else {
+					messageOne.textContent = data.location;
+					messageTwo.textContent = data.forecast;
+				}
+			});
+		})
+		.catch(err => console.log("Error: ", err));
+});
