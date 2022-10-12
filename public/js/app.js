448bc241516d1726989f93cfde06688bf996cee3
Jason Taylor <jasonleetaylor88@gmail.com>
2022-10-12 08:32:43 +0100

    fix: remove statically typed front end domain and unused constant

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherform.addEventListener("submit", async e => {
 	messageOne.textContent = "Loading...";
 	messageTwo.textContent = "";
 
-	fetch(`http://localhost:3000/weather?address=${location}`)
+	fetch(`/weather?address=${location}`)
 		.then(response => {
 			response.json().then(data => {
 				if (data.error) {
